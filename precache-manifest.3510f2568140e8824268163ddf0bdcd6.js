4a19515e307ed248decf0ad5577a47c87b83de28
Supan <supandeepsingh222@gmail.com>
2022-06-26 17:17:26 +0530

    Updates

---
+++ precache-manifest.3510f2568140e8824268163ddf0bdcd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaeccc7a4146870c2e6d75ab7d87ab4e",
+    "url": "/NotesApp/index.html"
+  },
+  {
+    "revision": "bd02386e704d4997b994",
+    "url": "/NotesApp/static/js/2.19196628.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/NotesApp/static/js/2.19196628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de72619cd96628c6bbb2",
+    "url": "/NotesApp/static/js/main.53e3fabb.chunk.js"
+  },
+  {
+    "revision": "0e0dd0ae63507c8d4b1a",
+    "url": "/NotesApp/static/js/runtime-main.a956eb1b.js"
+  }
+]);
